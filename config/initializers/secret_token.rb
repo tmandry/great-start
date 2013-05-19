c6256e6a523ac3931821b4f8ab30f06b6ceafb54
Tyler Mandry <tmandry@gmail.com>
2013-05-19 16:05:26 -0700

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Apollo::Application.config.secret_token = '06ca2f1cc92408cec43c2fe1d8eb5cf4c112edb795ac82ea20303377f6c30bb9eab594e12ff0a8bfbf2c5f17383e971d71a76b50306b86f33a812a0c65c7b6e6'
